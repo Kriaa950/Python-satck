49bffd7b95a1cad278322fa2e37311b1b13e7e12
ABDALLAH YASSINE KRIAA <150094152+Kriaa950@users.noreply.github.com>
2024-10-30 18:49:40 +0100

    Dojo Pets

--- Week 1/Day_2/Practice/Dojo Pets/main.py
+++ Week 1/Day_2/Practice/Dojo Pets/main.py
@@ -1,10 +1,9 @@
 from ninja import Ninja
 from pet import Pet
 
-my_pet = Pet("rex", "Dog", ["Fetch, roll over"])
-ninja = Ninja("Abdallah", "Ariaa", "dog Treats", "Dog food", my_pet)
+my_pet = Pet("Buddy", "Dog", ["sit", "roll", "over"])
+ninja = Ninja ("Abdallah" , "kriaa", ["bone", "grandor"], "food", my_pet)
 
 ninja.walk()
 ninja.feed()
 ninja.bathe()
-
--- Week 1/Day_2/Practice/Dojo Pets/pet.py
+++ Week 1/Day_2/Practice/Dojo Pets/pet.py
@@ -1,31 +1,19 @@
-# Create a Pet class with the pet attributes listed above
 class Pet:
     def __init__(self, name, type, tricks, health=100, energy=100):
-        self.name = name 
-        self.type = type 
+        self.name = name
+        self.type = type
         self.tricks = tricks
         self.health = health
         self.energy = energy
+
     def sleep(self):
-        print(f"{self.name} is sleeping.")
         self.energy += 25
-        return self
-    
+
     def eat(self):
-        print(f"{self.name} is eating.")
-        self.energy += 5
-        self.health += 10
-        print(self.energy)
-        print("yumuuuy", self.health)
-        return self
-    
+        self.energy +=5
+        self.energy += 10
+
     def play(self):
-        print(f"{self.name} is playing.")
         self.health += 5
-        print(self.health)
-        return self
-    
     def noise(self):
-        print(f"{self.name} is noisy!.")
-        return self
-        
+        print(f"{self.name} make a noise!")  # to test the function 
